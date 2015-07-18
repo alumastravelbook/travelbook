908a040c966b97291b9058ad507ee9c256384662
Tal Vintrob <tvaintrob@gmail.com>
2015-07-18 15:31:06 +0300

    deployed to heroku

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('travelbookApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
